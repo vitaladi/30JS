52a93878673f5701156eca08affe0131f0e4dc8f
Adimale <adimalevital@gmail.com>
2023-01-31 11:36:52 +0100

    Initial commit

---
+++ keymusic/drumKit.js
@@ -0,0 +1,16 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; // stop the function from running all together
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
+  key.classList.add("playing"); // add the class playing to the key
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; // skip it if it's not a transform
+  this.classList.remove("playing");
+}
+const keys = document.querySelectorAll(".key"); // select all the keys
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition)); // listen for the transitionend event on each key
+window.addEventListener("keydown", playSound);
